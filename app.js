e3dd531f447d2c84b7306e353dc46f862a035109
PaulNorthern <sculltakers111@gmail.com>
2020-07-10 19:03:59 +0300

    finished v1.0

--- app.js
+++ app.js
@@ -19,14 +19,16 @@ searchUser.addEventListener('keyup', (e) => {
             .then(data => {
                 if (data.profile.message === 'Not Found') {
                     // Show alert
-                    
+                    ui.showAlert('User not found', 'alert alert-danger');
                 } else {
                     // Show profile
                     ui.showProfile(data.profile);
+                    ui.showRepos(data.repos);
                 }
             })
     } else {
         // Clear Profile
+        ui.clearProfile();
 
     }
 });
